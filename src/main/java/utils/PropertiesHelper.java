5009032fa6410d2a33b4be39562528cb4c27b6f9
katyaZhuk <katya310789@mail.ru>
2021-09-29 13:34:49 +0200

    Added changes after review

--- src/main/java/utils/PropertiesHelper.java
+++ src/main/java/utils/PropertiesHelper.java
@@ -7,29 +7,72 @@ import java.util.Properties;
 public class PropertiesHelper {
     static Properties props = new Properties();
 
-    public static String getHost() throws IOException {
+    public static void loadConnectionFile () throws IOException {
         props.load(new FileInputStream("src/main/resources/connection.properties"));
+    }
+
+    public static void loadCreds () throws IOException {
+        props.load(new FileInputStream("src/main/resources/credentials.properties"));
+    }
+
+    public static String getHost() throws IOException {
+        loadConnectionFile();
         return props.getProperty("host");
     }
 
     public static String getDBName() throws IOException {
-        props.load(new FileInputStream("src/main/resources/connection.properties"));
+        loadConnectionFile();
         return props.getProperty("DBname");
     }
 
     public static String getURL() throws IOException {
-        props.load(new FileInputStream("src/main/resources/connection.properties"));
+        loadConnectionFile();
         return props.getProperty("url");
     }
 
     public static String getUser() throws IOException {
-        props.load(new FileInputStream("src/main/resources/connection.properties"));
+        loadConnectionFile();
         return props.getProperty("user");
     }
 
     public static String getPassword() throws IOException {
-        props.load(new FileInputStream("src/main/resources/connection.properties"));
+        loadConnectionFile();
         return props.getProperty("password");
+    }
+
+    public static String getID() throws IOException {
+        loadCreds();
+        return props.getProperty("id");
+    }
+
+    public static String getFirstName() throws IOException {
+        loadCreds();
+        return props.getProperty("first_name");
+    }
+
+    public static String getLastName() throws IOException {
+        loadCreds();
+        return props.getProperty("last_name");
+    }
+
+    public static String getLastCountry() throws IOException {
+        loadCreds();
+        return props.getProperty("last_country");
+    }
+
+    public static String getExpectedCountry() throws IOException {
+        loadCreds();
+        return props.getProperty("expected_country");
+    }
+
+    public static String getExpectedLastName() throws IOException {
+        loadCreds();
+        return props.getProperty("expected_last_name");
+    }
+
+    public static String getJoinCountry() throws IOException {
+        loadCreds();
+        return props.getProperty("join_country");
     }
 
 }
--- src/test/java/tests/DataBaseTest.java
+++ src/test/java/tests/DataBaseTest.java
@@ -3,11 +3,15 @@ package tests;
 import db_connection.JDBCConnection;
 import org.junit.jupiter.api.*;
 
+import java.io.IOException;
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static db_connection.JDBCConnection.connectToDB;
+import static org.junit.jupiter.api.Assertions.*;
+import static utils.PropertiesHelper.*;
+import static utils.PropertiesHelper.getJoinCountry;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -17,7 +21,7 @@ public class DataBaseTest {
     @Order(1)
     public void createTableTest() {
         String query = "CREATE TABLE actors (" +
-                "ID int(6) NOT NULL AUTO_INCREMENT," +
+                "ID int(6) NOT NULL," +
                 "FIRST_NAME VARCHAR(45) NOT NULL," +
                 "LAST_NAME VARCHAR(45) NOT NULL," +
                 "COUNTRY VARCHAR(45)," +
@@ -27,70 +31,84 @@ public class DataBaseTest {
 
     @Test
     @Order(2)
-    public void insertRequestTest() {
-        String query = "INSERT INTO actors (FIRST_NAME, LAST_NAME, COUNTRY)" +
-                "VALUES ('Merline', 'Monro', 'USA')";
+    public void insertRequestTest() throws IOException {
+        String query = "INSERT INTO actors (ID, FIRST_NAME, LAST_NAME, COUNTRY)" +
+                "VALUES ('" + getID() +
+                "', '" + getFirstName() +
+                "', '" + getLastName() +
+                "', '" + getLastCountry() + "')";
         JDBCConnection.insertIntoTable(query);
 
         String selectQuery = "SELECT * FROM actors";
         ResultSet rs = JDBCConnection.selectFromTable(selectQuery);
         assertAll("Should return inserted data",
-                () -> assertEquals("Merline", rs.getString("FIRST_NAME")),
-                () -> assertEquals("Monro", rs.getString("LAST_NAME")),
-                () -> assertEquals("USA", rs.getString("COUNTRY")));
+                () -> assertEquals(getID(), rs.getString("ID")),
+                () -> assertEquals(getFirstName(), rs.getString("FIRST_NAME")),
+                () -> assertEquals(getLastName(), rs.getString("LAST_NAME")),
+                () -> assertEquals(getLastCountry(), rs.getString("COUNTRY")));
     }
 
     @Test
     @Order(3)
-    public void updateRequestTest() throws SQLException {
+    public void updateRequestTest() throws SQLException, IOException {
         String query = "UPDATE actors " +
-                "SET COUNTRY = 'Canada' " +
-                "WHERE ID='1'";
+                "SET COUNTRY = '" + getExpectedCountry() + "' " +
+                "WHERE ID='" + getID() + "'";
         JDBCConnection.updateTable(query);
 
         String selectQuery = "SELECT COUNTRY FROM actors " +
-                "WHERE ID='1'";
+                "WHERE ID='" + getID() + "'";
         ResultSet rs = JDBCConnection.selectFromTable(selectQuery);
-        String expectedCountry = "Canada";
         String actualCountry = rs.getString("COUNTRY");
-        assertEquals(expectedCountry, actualCountry);
+        assertEquals(getExpectedCountry(), actualCountry);
     }
 
     @Test
     @Order(4)
-    public void deleteRequestTest() {
-        String query = "DELETE FROM actors WHERE ID='1'";
+    public void deleteRequestTest() throws SQLException, IOException {
+        String query = "DELETE FROM actors WHERE ID='" + getID() + "'";
         JDBCConnection.deleteFromTable(query);
+
+        String selectQuery = "SELECT COUNT(ID) FROM actors WHERE ID='" + getID() + "'";
+        ResultSet rs = JDBCConnection.selectFromTable(selectQuery);
+        assertEquals(0, rs.getInt("COUNT(ID)"));
     }
 
     @Test
     @Order(5)
-    public void dropTableTest() {
+    public void dropTableTest() throws SQLException {
         JDBCConnection.dropTable("actors");
+
+        DatabaseMetaData md = connectToDB().getMetaData();
+        ResultSet rs = md.getTables(null, null, "actors", null);
+        int countActorsTables = 0;
+        if (rs.next()) {
+            countActorsTables ++;
+        }
+
+        assertEquals(0, countActorsTables);
     }
 
     @Test
     @Order(6)
-    public void selectWithConditionTest() throws SQLException {
+    public void selectWithConditionTest() throws SQLException, IOException {
         String selectQuery = "SELECT * FROM actor " +
-                "WHERE last_name='DEPP'";
+                "WHERE last_name='" + getExpectedLastName() + "'";
         ResultSet rs = JDBCConnection.selectFromTable(selectQuery);
-        String expectedLastName = "DEPP";
         String actualCLastName = rs.getString("last_name");
-        assertEquals(expectedLastName, actualCLastName);
+        assertEquals(getExpectedLastName(), actualCLastName);
     }
 
     @Test
     @Order(7)
-    public void selectWithJoinTest() throws SQLException {
+    public void selectWithJoinTest() throws SQLException, IOException {
         String selectQuery = "SELECT city.*, country.country from city " +
                 "left join country " +
                 "on city.country_id = country.country_id " +
-                "where country = 'Belarus'";
+                "where country = '" + getJoinCountry() + "'";
         ResultSet rs = JDBCConnection.selectFromTable(selectQuery);
-        String expectedCountry = "Belarus";
         String actualCountry = rs.getString("country");
-        assertEquals(expectedCountry, actualCountry);
+        assertEquals(getJoinCountry(), actualCountry);
     }
 
 }
